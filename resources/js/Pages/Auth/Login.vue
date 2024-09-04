6d83927933bfee1c7278636b6d88ce4dcbc8693e
Jean Becerra <j.becerra.22.jhire@gmail.com>
2024-09-04 09:39:59 -0500

    Translate Profile Options to Spanish

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -34,6 +34,7 @@ function redirectToGoogle() {
 </script>
 
 <template>
+
     <Head title="Log in" />
 
     <AuthenticationCard>
@@ -45,44 +46,28 @@ function redirectToGoogle() {
         </div>
         <div class="pb-5 text-sm text-center text-gray-600">
             ¿Aun no tienes una cuenta?
-            <Link
-                :href="route('register')"
-                class="font-semibold text-gray-800 hover:text-gray-600 hover:underline"
-            >
-                Registrate aquí
+            <Link :href="route('register')" class="font-semibold text-gray-800 hover:text-gray-600 hover:underline">
+            Registrate aquí
             </Link>
         </div>
 
-            <div class="mb-4 text-center">
-            <button
-                @click="redirectToGoogle"
-                class="inline-flex w-full px-4 py-2 pl-32 text-sm font-semibold text-gray-800 bg-white border border-gray-300 hover:bg-gray-100 focus:bg-gray-100"
-            >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 48 48"
-                >
-                    <path
-                        fill="#fbc02d"
-                        d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                    ></path>
-                    <path
-                        fill="#e53935"
-                        d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                    ></path>
-                    <path
-                        fill="#4caf50"
-                        d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                    ></path>
-                    <path
-                        fill="#1565c0"
-                        d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                    ></path></svg
-                >&ensp;Continuar con Google
+        <div class="mb-4 text-center">
+            <button @click="redirectToGoogle"
+                class="inline-flex w-full px-4 py-2 pl-32 text-sm font-semibold text-gray-800 bg-white border border-gray-300 hover:bg-gray-100 focus:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
+                    <path fill="#fbc02d"
+                        d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z">
+                    </path>
+                    <path fill="#e53935"
+                        d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z">
+                    </path>
+                    <path fill="#4caf50"
+                        d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z">
+                    </path>
+                    <path fill="#1565c0"
+                        d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z">
+                    </path>
+                </svg>&ensp;Continuar con Google
             </button>
         </div>
         <div class="flex items-center my-4">
@@ -98,46 +83,27 @@ function redirectToGoogle() {
         <form @submit.prevent="submit">
             <div>
                 <InputLabel for="email" value="Correo Electrónico" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="block w-full mt-1"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                <TextInput id="email" v-model="form.email" type="email" class="block w-full mt-1" required autofocus
+                    autocomplete="username" />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="password" value="Contraseña" />
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="block w-full mt-1"
-                    required
-                    autocomplete="current-password"
-                />
+                <TextInput id="password" v-model="form.password" type="password" class="block w-full mt-1" required
+                    autocomplete="current-password" />
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="text-sm text-gray-600 ms-2"
-                        >Mantener sesión activa</span
-                    >
+                    <span class="text-sm text-gray-600 ms-2">Mantener sesión activa</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <PrimaryButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
+                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Iniciar Sesión
                 </PrimaryButton>
             </div>
